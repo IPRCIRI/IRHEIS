0eefafbd9275719d33e73022609fc64bf41a2e91
arin1989 <arin1989@gmail.com>
2017-11-05 22:57:10 +0330

    Add files to calculate Food_Calories

---
+++ R/21-HHBerenjs.R
@@ -0,0 +1,51 @@
+# 21-HHBerenjs.R
+# Builds the Berenj expenditures data.table for households
+#
+# Copyright © 2017: Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ HHBerenjs =====================================\n")
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(data.table)
+library(stringr)
+library(readxl)
+
+
+BerenjTables <- data.table(read_excel(Settings$MetaDataFilePath,sheet=Settings$MDS_Berenj))
+
+
+
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
+  ft <- BerenjTables[Year==year]
+  tab <- ft$Table
+  if(is.na(tab))
+    next
+  UTF <- Tables[[paste0("U",year,tab)]]
+  RTF <- Tables[[paste0("R",year,tab)]]
+  TF <- rbind(UTF,RTF)
+  for(n in names(TF)){
+    x <- which(ft==n)
+    if(length(x)>0)
+      setnames(TF,n,names(ft)[x])
+  }
+  pcols <- intersect(names(TF),c("HHID","Code","BerenjExpenditure"))
+  TF <- TF[,pcols,with=FALSE]
+  TF <- TF[Code %in% ft$StartCode:ft$EndCode]
+  if(year %in% 84:94){
+    TF[,BerenjExpenditure:=as.numeric(BerenjExpenditure)]
+  }
+  TF[,Code:=NULL]
+  TF[is.na(TF)] <- 0
+  BerenjData <- TF[,lapply(.SD,sum),by=HHID]
+  save(BerenjData, file = paste0(Settings$HEISProcessedPath,"Y",year,"Berenjs.rda"))
+}
+endtime <- proc.time()
+cat("\n\n============================\nIt took ")
+cat(endtime-starttime)
