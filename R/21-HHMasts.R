534744c624d5b1d061115eb9343297904877a774
arin1989 <arin1989@gmail.com>
2017-11-05 22:57:10 +0330

    Add files to calculate Food_Calories

---
+++ R/21-HHMasts.R
@@ -0,0 +1,51 @@
+# 21-HHMasts.R
+# Builds the Mast expenditures data.table for households
+#
+# Copyright © 2017: Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ HHMasts =====================================\n")
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(data.table)
+library(stringr)
+library(readxl)
+
+
+MastTables <- data.table(read_excel(Settings$MetaDataFilePath,sheet=Settings$MDS_Mast))
+
+
+
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
+  ft <- MastTables[Year==year]
+  tab <- ft$Table
+  if(is.na(tab))
+    next
+  UTF <- Tables[[paste0("U",year,tab)]]
+  RTF <- Tables[[paste0("R",year,tab)]]
+  TF <- rbind(UTF,RTF)
+  for(n in names(TF)){
+    x <- which(ft==n)
+    if(length(x)>0)
+      setnames(TF,n,names(ft)[x])
+  }
+  pcols <- intersect(names(TF),c("HHID","Code","MastExpenditure"))
+  TF <- TF[,pcols,with=FALSE]
+  TF <- TF[Code %in% ft$StartCode:ft$EndCode]
+  if(year %in% 84:94){
+    TF[,MastExpenditure:=as.numeric(MastExpenditure)]
+  }
+  TF[,Code:=NULL]
+  TF[is.na(TF)] <- 0
+  MastData <- TF[,lapply(.SD,sum),by=HHID]
+  save(MastData, file = paste0(Settings$HEISProcessedPath,"Y",year,"Masts.rda"))
+}
+endtime <- proc.time()
+cat("\n\n============================\nIt took ")
+cat(endtime-starttime)
---
+++ R/21-HHSabzis.R
@@ -0,0 +1,51 @@
+# 21-HHSabzis.R
+# Builds the Sabzi expenditures data.table for households
+#
+# Copyright © 2017: Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ HHSabzis =====================================\n")
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(data.table)
+library(stringr)
+library(readxl)
+
+
+SabziTables <- data.table(read_excel(Settings$MetaDataFilePath,sheet=Settings$MDS_Sabzi))
+
+
+
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
+  ft <- SabziTables[Year==year]
+  tab <- ft$Table
+  if(is.na(tab))
+    next
+  UTF <- Tables[[paste0("U",year,tab)]]
+  RTF <- Tables[[paste0("R",year,tab)]]
+  TF <- rbind(UTF,RTF)
+  for(n in names(TF)){
+    x <- which(ft==n)
+    if(length(x)>0)
+      setnames(TF,n,names(ft)[x])
+  }
+  pcols <- intersect(names(TF),c("HHID","Code","SabziExpenditure"))
+  TF <- TF[,pcols,with=FALSE]
+  TF <- TF[Code %in% ft$StartCode:ft$EndCode]
+  if(year %in% 84:94){
+    TF[,SabziExpenditure:=as.numeric(SabziExpenditure)]
+  }
+  TF[,Code:=NULL]
+  TF[is.na(TF)] <- 0
+  SabziData <- TF[,lapply(.SD,sum),by=HHID]
+  save(SabziData, file = paste0(Settings$HEISProcessedPath,"Y",year,"Sabzis.rda"))
+}
+endtime <- proc.time()
+cat("\n\n============================\nIt took ")
+cat(endtime-starttime)
