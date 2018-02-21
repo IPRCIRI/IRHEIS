97cbffa27c6ff031dc8a09733aad35c1a356a4c7
Majid Einian <einian85@gmail.com>
2018-02-21 21:29:29 +0330

    Data Munging (NOT COMPLETE)

--- R/70-ActualPanel.R
+++ R/70-ActualPanel.R
@@ -18,57 +18,48 @@ library(data.table)
 year <- 92
 load(paste0(Settings$HEISProcessedPath,"Y",year,"HHBase.rda"))
 load(paste0(Settings$HEISProcessedPath,"Y",year,"HHI.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",year,"PubWage.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",year,"PrvWages.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",year,"bussWages.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",year,"AgriWages.rda"))
-load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,92,".rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"Total_Income.rda"))
+load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,year,".rda"))
 D92 <- merge(HHBase,HHI,by="HHID", all.x=TRUE)
-D92 <- merge(D92,PubWageData,by="HHID", all.x=TRUE)
-D92 <- merge(D92,PrvWageData,by="HHID", all.x=TRUE)
+D92 <- merge(D92,MyIncome,by="HHID", all.x=TRUE)
 D92 <- merge(D92,HHWeights[,.(HHID,Weight)],by="HHID", all.x=TRUE)
-D92[pubsection==1, WorkClass:="Govt"]
-D92[is.na(pubsection) & prvsection=="Agriculture", WorkClass:="Agg"]
-D92[is.na(WorkClass) & HNonWageSector=="NonAgriculture", WorkClass:="Buss"]
-D92[is.na(HNonWageSector) & HWageWorkType %in% c("Priv","Coop") , WorkClass:="Priv"]
-D92[is.na(HNonWageSector) & is.na(HWageWorkType), WorkClass:="Retr"]
-
+D92[is.na(Sector), Sector:="Rtr"]
 rm(HHBase,HHI,PubWageData,PrvWageData,HHWeights)
 
 
-load(paste0(Settings$HEISProcessedPath,"Y",93,"HHBase.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",93,"HHI.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",93,"PubWage.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",93,"PrvWages.rda"))
-load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,93,".rda"))
+year <- 93
+load(paste0(Settings$HEISProcessedPath,"Y",year,"HHBase.rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"HHI.rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"Total_Income.rda"))
+load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,year,".rda"))
 D93 <- merge(HHBase,HHI,by="HHID", all.x=TRUE)
-D93 <- merge(D93,PubWageData,by="HHID", all.x=TRUE)
-D93 <- merge(D93,PrvWageData,by="HHID", all.x=TRUE)
+D93 <- merge(D93,MyIncome,by="HHID", all.x=TRUE)
 D93 <- merge(D93,HHWeights[,.(HHID,Weight)],by="HHID", all.x=TRUE)
+D93[is.na(Sector), Sector:="Rtr"]
 rm(HHBase,HHI,PubWageData,PrvWageData,HHWeights)
 
 
-load(paste0(Settings$HEISProcessedPath,"Y",94,"HHBase.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",94,"HHI.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",94,"PubWage.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",94,"PrvWages.rda"))
-load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,94,".rda"))
+year <- 94
+load(paste0(Settings$HEISProcessedPath,"Y",year,"HHBase.rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"HHI.rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"Total_Income.rda"))
+load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,year,".rda"))
 D94 <- merge(HHBase,HHI,by="HHID", all.x=TRUE)
-D94 <- merge(D94,PubWageData,by="HHID", all.x=TRUE)
-D94 <- merge(D94,PrvWageData,by="HHID", all.x=TRUE)
+D94 <- merge(D94,MyIncome,by="HHID", all.x=TRUE)
 D94 <- merge(D94,HHWeights[,.(HHID,Weight)],by="HHID", all.x=TRUE)
+D94[is.na(Sector), Sector:="Rtr"]
 rm(HHBase,HHI,PubWageData,PrvWageData,HHWeights)
 
 
-load(paste0(Settings$HEISProcessedPath,"Y",95,"HHBase.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",95,"HHI.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",95,"PubWage.rda"))
-load(paste0(Settings$HEISProcessedPath,"Y",95,"PrvWages.rda"))
-load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,95,".rda"))
+year <- 95
+load(paste0(Settings$HEISProcessedPath,"Y",year,"HHBase.rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"HHI.rda"))
+load(paste0(Settings$HEISProcessedPath,"Y",year,"Total_Income.rda"))
+load(paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,year,".rda"))
 D95 <- merge(HHBase,HHI,by="HHID", all.x=TRUE)
-D95 <- merge(D95,PubWageData,by="HHID", all.x=TRUE)
-D95 <- merge(D95,PrvWageData,by="HHID", all.x=TRUE)
+D95 <- merge(D95,MyIncome,by="HHID", all.x=TRUE)
 D95 <- merge(D95,HHWeights[,.(HHID,Weight)],by="HHID", all.x=TRUE)
+D95[is.na(Sector), Sector:="Rtr"]
 rm(HHBase,HHI,PubWageData,PrvWageData,HHWeights)
 
 
@@ -80,13 +71,7 @@ P234 <- merge(P23,D94,by="HHID",all = FALSE)
 P345 <- merge(P34,D95,by="HHID",all = FALSE)
 
 P <- P23
-P[is.na(pubsection.x),pubsection.x:=0]
-P[is.na(prvsection.x),prvsection.x:=0]
-P[is.na(pubsection.y),pubsection.y:=0]
-P[is.na(prvsection.y),prvsection.y:=0]
 
-P[,Sect.x:=pubsection.x+10*prvsection.x]
-P[,Sect.y:=pubsection.y+10*prvsection.y]
 
 table(P[,.(Region.x,Region.y)])
 summary(P[,Quarter.x-Quarter.y])
@@ -94,7 +79,8 @@ summary(P[,Month.x-Month.y])
 table(P[,.(Month.x,Month.y)])
 table(P[,.(ProvinceCode.x-ProvinceCode.y)])
 table(P[,.(HActivityState.x,HActivityState.y)])
-table(P[,.(Sect.x,Sect.y)])
+table(P[,.(Sector.x,Sector.y)])
+table(P[,.(Sector.x,HActivityState.y)])
 L <- P[,.(sum(Weight.x,na.rm = TRUE),sum(Weight.y, na.rm = TRUE)),by=.(Sect.x,Sect.y)][order(Sect.x,Sect.y)]
 
 
