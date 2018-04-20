cad6094a385302d30f19183f99a66e8aee60a658
Majid Einian <einian85@gmail.com>
2018-04-20 21:22:17 +0430

    Updated reading other sources of income

--- R/35-OtherIncome.R
+++ R/35-OtherIncome.R
@@ -1,7 +1,7 @@
 # 35-OtherIncome.R
 # Builds the OtherIncome data.table for households
 #
-# Copyright © 2017: Arin Shahbazian
+# Copyright © 2018: Arin Shahbazian, Majid Einian
 # Licence: GPL-3
 
 rm(list=ls())
@@ -16,44 +16,53 @@ library(stringr)
 library(readxl)
 
 
-OtherWageTable <- data.table(read_excel(Settings$MetaDataFilePath,sheet=Settings$MDS_OtherWage))
+OtherIncomeTable <- data.table(read_excel(Settings$MetaDataFilePath,
+                                          sheet=Settings$MDS_OtherInc))
 
 
 for(year in (Settings$startyear:Settings$endyear)){
   cat(paste0("\n------------------------------\nYear:",year,"\n"))
   load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
-  Otherwt <- OtherWageTable[Year==year]
-  tab <- Otherwt$Table
+  OtherITb <- OtherIncomeTable[Year==year]
+  tab <- OtherITb$Table
   if(is.na(tab))
     next
-  UTOtherW <- Tables[[paste0("U",year,tab)]]
-  RTOtherW <- Tables[[paste0("R",year,tab)]]
-  TOtherW <- rbind(UTOtherW,RTOtherW,fill=TRUE)
-  for(n in names(TOtherW)){
-    x <- which(Otherwt==n)
-    if(length(x)>0)
-      setnames(TOtherW,n,names(Otherwt)[x])
+  UTOtherInc <- Tables[[paste0("U",year,tab)]]
+  RTOtherInc <- Tables[[paste0("R",year,tab)]]
+  TOtherInc <- rbind(UTOtherInc,RTOtherInc,fill=TRUE)
+  TOtherInc <- TOtherInc[,lapply(.SD, as.integer)]
+  if(!is.na(OtherITb$Code)){  # 63 to 68
+    setnames(TOtherInc,OtherITb$HHID,"HHID")
+    setnames(TOtherInc,OtherITb$Code,"Code")
+    setnames(TOtherInc,OtherITb$Value,"Value")
+    TOtherInc[Code %in% eval(parse(text = OtherITb$Retirement)),
+              Retirement:=sum(Value),by=HHID]
+    TOtherInc[Code %in% eval(parse(text = OtherITb$Rent)),
+              Rent:=sum(Value),by=HHID]
+    TOtherInc[Code %in% eval(parse(text = OtherITb$Interest)),
+              Interest:=sum(Value),by=HHID]
+    TOtherInc[Code %in% eval(parse(text = OtherITb$Aid)),
+              Aid:=sum(Value),by=HHID]
+    TOtherInc[Code %in% eval(parse(text = OtherITb$HomeProduction)),
+              HomeProduction:=sum(Value),by=HHID]
+  }else{ # 69+
+    for(n in names(TOtherInc)){
+      x <- which(OtherITb==n)
+      if(length(x)>0)
+        setnames(TOtherInc,n,names(OtherITb)[x])
+    }
   }
-  pcols <- intersect(names(TOtherW),c("HHID","Code","retirement","rent","interest","aid","homemade","intra"))
-  TOtherW <- TOtherW[,pcols,with=FALSE]
-  
- # if(year %in% 63:68){
-  #  if(Code==511){  
-  #    TOtherW <- TOtherW[ retirement ==2 ] 
- # } else if(Code==512:514){ 
-   #   TOtherW <- TOtherW[ rent ==2 ] 
- # } else if(Code==515:518){ 
-   # TOtherW <- TOtherW[ interest ==2 ] 
- # } else if(Code==521:522){ 
- #   TOtherW <- TOtherW[ aid ==2 ] 
- # } else if(Code==519){ 
- #   TOtherW <- TOtherW[ homemade ==2 ] 
- # } 
-#}
-  TOtherW[is.na(TOtherW)] <- 0
-  # OtherWageData <- TOtherW[,lapply(.SD,sum),by=HHID]
-  # save(OtherWageData, file = paste0(Settings$HEISProcessedPath,"Y",year,"OtherWage.rda"))
+  pcols <- intersect(names(TOtherInc),
+                     c("HHID","Code","Retirement","Rent",
+                       "Interest","Aid","HomeProduction","Intra"))
+  TOtherInc <- TOtherInc[,lapply(.SD, sum, na.rm=TRUE),by=HHID,.SDcols=pcols]
+  repna <- function(x) replace(x, which(is.na(x)), 0)
+  TOtherInc <- TOtherInc[,lapply(.SD, repna), .SDcols=pcols]
+  TOtherInc[, OtherIncome := Reduce(`+`, .SD), .SDcols=pcols]
+  save(TOtherInc, 
+       file = paste0(Settings$HEISProcessedPath,"Y",year,"OtherIncome.rda"))
 }
 endtime <- proc.time()
 cat("\n\n============================\nIt took ")
-cat(endtime-starttime)
+cat((endtime-starttime)[3])
+cat(" seconds.")
