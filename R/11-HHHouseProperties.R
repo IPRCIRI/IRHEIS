33099e12fba62e9afa4065da52393142d16f6ea7
arin1989 <arin1989@gmail.com>
2019-04-20 09:33:48 +0430

    Edit HEIS Part2 codes

--- R/11-HHHouseProperties.R
+++ R/11-HHHouseProperties.R
@@ -50,8 +50,154 @@ for(year in years){
 
   f <- function(x){as.numeric(str_trim(x))}
   P2 <- P2[, lapply(.SD, f)] 
-  HHHouseProperties<-P2
   
+  P2[is.na(P2)]<-0  
+  
+  P2[,tenure :=factor(tenure, levels=1:7, 
+                    labels=c("OwnLandandBuilding","Apartment","Rented",
+                             "Mortgage","AgainstService",
+                             "Free","Other"))]
+  
+  P2[,skeleton :=factor(skeleton, levels=1:3, 
+                      labels=c("metal","concrete","other"))]
+  
+  P2[,constmat :=factor(constmat, levels=1:8, 
+                      labels=c("BrickSteel_StoneSteel","Brickwood_Stonewood","CementBlocks",
+                               "AllBrick_Stone","Allwood",
+                               "SundriedBrickwood","SundriedBrickmud",
+                               "other"))]
+  
+  P2[,cookfuel :=factor(cookfuel, levels=1:10, 
+                        labels=c("karosine","gasoline",
+                                 "gas","pipedgas",
+                                 "electricity","woodandcharcoal",
+                                 "Animalfuel","charcoal","otherFuel","None"))]
+  
+  P2[,heatfuel :=factor(heatfuel, levels=11:20, 
+                        labels=c("karosine","gasoline",
+                                 "gas","pipedgas",
+                                 "electricity","woodandcharcoal",
+                                 "Animalfuel","charcoal","otherFuel","None"))]
+  
+  P2[,hotwater :=factor(hotwater, levels=21:30, 
+                        labels=c("karosine","gasoline",
+                                 "gas","pipedgas",
+                                 "electricity","woodandcharcoal",
+                                 "Animalfuel","charcoal","otherFuel","None"))]
+  
+  P2[,car := factor(car, levels = 0:1,
+                    labels=c("False","True"))]
+  
+  P2[,motorcycle := factor(motorcycle, levels=0:1,
+                    labels=c("False","True"))]
+  
+  P2[,bike := factor(bike, levels=0:1,
+                           labels=c("False","True"))]
+
+  P2[,radio := factor(radio, levels=0:1,
+                           labels=c("False","True"))]
+  
+  P2[,cassette := factor(cassette, levels=0:1,
+                           labels=c("False","True"))]
+  
+  P2[,tvbw := factor(tvbw, levels=0:1,
+                         labels=c("False","True"))]
+  
+  P2[,tvcr := factor(tvcr, levels=0:1,
+                         labels=c("False","True"))]
+  
+  P2[,vcr := factor(vcr, levels=0:1,
+                     labels=c("False","True"))]
+  
+  P2[,computer := factor(computer, levels=0:1,
+                     labels=c("False","True"))]
+  
+  P2[,cellphone := factor(cellphone, levels=0:1,
+                     labels=c("False","True"))]
+  
+  P2[,freezer := factor(freezer, levels=0:1,
+                          labels=c("False","True"))]
+  
+  P2[,refrigerator := factor(refrigerator, levels=0:1,
+                          labels=c("False","True"))]
+  
+  P2[,frez_refrig := factor(frez_refrig, levels=0:1,
+                          labels=c("False","True"))]
+  
+  P2[,oven := factor(oven, levels=0:1,
+                            labels=c("False","True"))]
+  
+  P2[,vacuum := factor(vacuum, levels=0:1,
+                            labels=c("False","True"))]
+  
+  P2[,washer := factor(washer, levels=0:1,
+                            labels=c("False","True"))]
+  
+  P2[,sewing := factor(sewing, levels=0:1,
+                            labels=c("False","True"))]
+  
+  P2[,fan := factor(fan, levels=0:1,
+                       labels=c("False","True"))]
+  
+  P2[,cooler_water_movable := factor(cooler_water_movable,
+                                     levels=0:1,
+                       labels=c("False","True"))]
+  
+  P2[,cooler_gas_movable := factor(cooler_gas_movable, 
+                                   levels=0:1,
+                       labels=c("False","True"))]
+  
+  P2[,dishwasher := factor(dishwasher, levels=0:1,
+                       labels=c("False","True"))]
+  
+  if(year %in% 91:96){
+  P2[,Microwave := factor(Microwave, levels=0:1,
+                           labels=c("False","True"))]
+  }
+  
+  P2[,none := factor(none, levels=0:1,
+                           labels=c("False","True"))]
+
+  P2[,pipewater := factor(pipewater, levels=0:1,
+                     labels=c("False","True"))]
+  
+  P2[,electricity := factor(electricity, levels=0:1,
+                     labels=c("False","True"))]
+  
+  P2[,pipegas := factor(pipegas, levels=0:1,
+                     labels=c("False","True"))]
+  
+  P2[,phone := factor(phone, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,internet := factor(internet, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,bathroom := factor(bathroom, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,kitchen := factor(kitchen, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,cooler := factor(cooler, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,centralcooler := factor(centralcooler, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,centralheat := factor(centralheat, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,pakage := factor(pakage, levels=0:1,
+                        labels=c("False","True"))]
+  
+  P2[,cooler_gas := factor(cooler_gas, levels=0:1,
+                       labels=c("False","True"))]
+  
+  P2[,ego := factor(ego, levels=0:1,
+                       labels=c("False","True"))]
+  
+  HHHouseProperties<-P2
   save(HHHouseProperties, file=paste0(Settings$HEISProcessedPath,"Y",year,"HHHouseProperties.rda"))
   
 }
