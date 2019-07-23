6b259bd0d0160cffe08c7bdf8ac9b83a085ae6f9
zahrashahidi <zha.shahidi@gmail.com>
2019-07-23 12:09:51 +0430

    test

---
+++ t.R
@@ -0,0 +1,18 @@
+# 57-PovertyLines.R
+# 
+# Copyright © 2018:Majid Einian & Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ Prepare Data =====================================\n")
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(readxl)
+library(data.table)
+library(ggplot2)
+library(stats)
+
+f
