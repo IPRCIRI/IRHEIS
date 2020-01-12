e4b0f26546fe8a52139ac1c34a3500af28ce0904
Arin Shahbazian <arin1989@gmail.com>
2020-01-12 12:05:04 +0330

    Some updates in code to extract graphs

--- R/177-Step 7 ,8,9-PovertyLines.R
+++ R/177-Step 7 ,8,9-PovertyLines.R
@@ -27,6 +27,15 @@ FinalClusterResults <- data.table(Year=NA_integer_,cluster3=NA_integer_,MetrPric
                                   PovertyLine=NA_real_,PovertyHCR=NA_real_,
                                   PovertyGap=NA_real_,PovertyDepth=NA_real_)[0]
 
+FinalClusterDiff <- data.table(Year=NA_integer_,cluster3=NA_integer_,MetrPrice=NA_real_,
+                                  House_Share=NA_real_,Food_Share=NA_real_,Durable_Share=NA_real_,
+                                  SampleSize=NA_integer_,Clusterdiff=NA_integer_,
+                                  Engle=NA_integer_,FPLine=NA_integer_,
+                                  Total_Exp_Month_Per_nondurable=NA_real_,
+                                  Total_Exp_Month_Per=NA_real_,
+                                  PovertyLine=NA_real_,PovertyHCR=NA_real_,
+                                  PovertyGap=NA_real_,PovertyDepth=NA_real_)[0]
+
 for(year in (Settings$startyear:Settings$endyear)){
   cat(paste0("\nYear:",year,"\t"))
   
@@ -34,6 +43,8 @@ for(year in (Settings$startyear:Settings$endyear)){
   load(file=paste0(Settings$HEISProcessedPath,"Y",year,"FinalFoodPoor.rda"))
   
   #MD<-MD[Region=="Rural"]
+  #MD<-MD[cluster3==7]
+  MD<-MD[,Clusterdiff:=ifelse(cluster3==7,1,0)]
  
   EngleD <- MD[ TOriginalFoodExpenditure_Per>0.8*FPLine & TOriginalFoodExpenditure_Per<1.2*FPLine,
                .(.N,Engel=weighted.mean(TOriginalFoodExpenditure/Total_Exp_Month,Weight),
@@ -71,6 +82,27 @@ for(year in (Settings$startyear:Settings$endyear)){
   X <- merge(X1,X2,by=c("Year","Region"))
   FinalRegionResults <- rbind(FinalRegionResults,X)
   
+  ################Cluster Diff##################
+  X1 <- MD[,.(SampleSize=.N,MetrPrice=weighted.mean(MetrPrice,Weight,na.rm = TRUE),
+              House_Share=weighted.mean(ServiceExp/Total_Exp_Month,Weight),
+              Food_Share=weighted.mean(FoodExpenditure/Total_Exp_Month,Weight),
+              Durable_Share=weighted.mean(Durable_Exp/Total_Exp_Month,Weight),
+              Engle=weighted.mean(FoodExpenditure/Total_Exp_Month,Weight),
+              FPLine=weighted.mean(FPLine,Weight),
+              Total_Exp_Month_Per_nondurable=weighted.mean(Total_Exp_Month_Per_nondurable,Weight),
+              Total_Exp_Month_Per=weighted.mean(Total_Exp_Month_Per,Weight),
+              cluster3=weighted.mean(cluster3,Weight),
+              PovertyLine=weighted.mean(PovertyLine,Weight*Size),
+              PovertyHCR=weighted.mean(FinalPoor,Weight*Size)),by=Clusterdiff]
+  X2 <- MD[FinalPoor==1,.(PovertyGap=weighted.mean(FGT1M,Weight*Size),
+                          PovertyDepth=weighted.mean(FGT2M,Weight*Size)),by=Clusterdiff]
+  
+  
+  X1[,Year:=year]
+  X2[,Year:=year]
+  X <- merge(X1,X2,by=c("Year","Clusterdiff"))
+  FinalClusterDiff <- rbind(FinalClusterDiff,X)
+  
   ################Cluster##################
   X1 <- MD[,.(SampleSize=.N,MetrPrice=weighted.mean(MetrPrice,Weight,na.rm = TRUE),
               House_Share=weighted.mean(ServiceExp/Total_Exp_Month,Weight),
@@ -104,6 +136,32 @@ save(FinalCountryResults,file=paste0(Settings$HEISProcessedPath,"FinalCountryRes
 ggplot(FinalClusterResults)+
   geom_line(mapping = aes(x=Year,y=log(MetrPrice),col=factor(cluster3)))
 
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=MetrPrice,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=FPLine,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=Engle,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=PovertyLine,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=House_Share,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=Food_Share,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=Durable_Share,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=Total_Exp_Month_Per_nondurable,col=factor(Clusterdiff)))
+
+ggplot(FinalClusterDiff)+
+  geom_line(mapping = aes(x=Year,y=Total_Exp_Month_Per,col=factor(Clusterdiff)))
 
 
 
