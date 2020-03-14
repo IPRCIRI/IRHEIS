511a4b4e187fb0d8cd10d36b058b355320c18736
Arin Shahbazian <arin1989@gmail.com>
2020-03-15 00:16:06 +0330

    According to the description
    Rename NewArea2 to NewArea_Name in all poverty codes + Some other updates in 174 and durable expenditures

--- R/174-Step4-RealDeciles.R
+++ R/174-Step4-RealDeciles.R
@@ -21,9 +21,9 @@ for(year in (Settings$startyear:Settings$endyear)){
   # load data --------------------------------------
   load(file=paste0(Settings$HEISProcessedPath,"Y",year,"Merged4CBN3.rda"))
   
-  SMD <- MD[,.(HHID,Region,
+  SMD <- MD[,.(HHID,Region,ProvinceCode,
                ServiceExp,FoodExpenditure,Total_Exp_Month,
-               NewArea,NewArea2,Total_Exp_Month_Per_nondurable,TOriginalFoodExpenditure_Per,
+               NewArea,NewArea_Name,Total_Exp_Month_Per_nondurable,TOriginalFoodExpenditure_Per,
                # Total_Exp_Month_Per_nondurable2,TFoodExpenditure_Per2,
                Durable_Exp,
                TFoodKCaloriesHH_Per,Calorie_Need_WorldBank,Calorie_Need_Anstitoo,
@@ -45,20 +45,20 @@ for(year in (Settings$startyear:Settings$endyear)){
   
   SMD[,PriceIndex2:=(weighted.mean(Bundle_Value,Weight,na.rm = TRUE)/TBV1
                     +weighted.mean(MetrPrice,Weight,na.rm = TRUE)/TBV2)/2
-      ,by=.(Region,NewArea2)]
+      ,by=.(Region,NewArea_Name)]
   
   X <- SMD[,.(weighted.mean(FoodExpenditure/Total_Exp_Month,Weight),
-             weighted.mean(ServiceExp/Total_Exp_Month,Weight)),by=.(Region,NewArea2)]
+             weighted.mean(ServiceExp/Total_Exp_Month,Weight)),by=.(Region,NewArea_Name)]
   X[,V:=V1+V2]
 
-  SMD <- merge(SMD,X,by=c("Region","NewArea2"))
+  SMD <- merge(SMD,X,by=c("Region","NewArea_Name"))
   
   SMD[,PriceIndex:=(weighted.mean(Bundle_Value,Weight,na.rm = TRUE)/TBV1*V1
                     +weighted.mean(MetrPrice,Weight,na.rm = TRUE)/TBV2*V2)/V
-      ,by=.(Region,NewArea2)]
+      ,by=.(Region,NewArea_Name)]
   
   Compare1<-SMD[,.(Old=mean(PriceIndex2),
-                  New=mean(PriceIndex)),by=.(Region,NewArea2)]
+                  New=mean(PriceIndex)),by=.(Region,NewArea_Name)]
 
   SMD[,V1:=NULL]
   SMD[,V2:=NULL]
@@ -92,20 +92,6 @@ for(year in (Settings$startyear:Settings$endyear)){
   SMD[,Percentile_Nominal:=cut(crw2,breaks=seq(0,1,.01),labels=1:100)]
   
   
- # C<-SMD[,.(.N,Max=max(Total_Exp_Month_Per_nondurable),
- #           Min=min(Total_Exp_Month_Per_nondurable),
- #           Mean=mean(Total_Exp_Month_Per_nondurable)),
- #        by=.(Region,NewArea,NewArea2,Decile)]
-  
- # D<-SMD[,.(.N,Mean=weighted.mean(Durable_Exp/Size,Weight)),
- #       by=.(Region,NewArea,NewArea2,Decile)]
-
- # B<-SMD[,.(.N,Mean=mean(MetrPrice)),
- #       by=.(Region,NewArea,NewArea2)]
-  
- # write.csv(B,file = "B.csv")
- # write.csv(C,file = "C.csv")
- # write.csv(D,file = "D.csv")
 
   SMD[,NewPoor:=1]
   SMD[,ThisIterationPoor:=0]
@@ -115,7 +101,7 @@ for(year in (Settings$startyear:Settings$endyear)){
     SMD[,pold:=Percentile]
     SMD[,ThisIterationPoor:=ifelse(pold %in% Settings$InitialPoorPercentile,1,0)]
     SMDIterationPoor<-SMD[ThisIterationPoor==TRUE]
-    SMDIterationPoor[,sum(ThisIterationPoor),by=.(Region,NewArea2)][order(Region,NewArea2)]
+    SMDIterationPoor[,sum(ThisIterationPoor),by=.(Region,NewArea_Name)][order(Region,NewArea_Name)]
     
     T_P_Bundle_Value <- SMDIterationPoor[NewArea==2301, .(Bundle_Value,MetrPrice,Weight)]
     TPBV1 <- T_P_Bundle_Value[,weighted.mean(Bundle_Value,Weight,na.rm = TRUE)]
@@ -123,20 +109,20 @@ for(year in (Settings$startyear:Settings$endyear)){
     
    
     X <- SMDIterationPoor[,.(weighted.mean(FoodExpenditure/Total_Exp_Month,Weight),
-                weighted.mean(ServiceExp/Total_Exp_Month,Weight)),by=.(Region,NewArea2)]
+                weighted.mean(ServiceExp/Total_Exp_Month,Weight)),by=.(Region,NewArea_Name)]
     X[,V:=V1+V2]
     
-    SMDIterationPoor <- merge(SMDIterationPoor,X,by=c("Region","NewArea2"))
+    SMDIterationPoor <- merge(SMDIterationPoor,X,by=c("Region","NewArea_Name"))
     SMDIterationPoor[,PriceIndex:=NULL]  
     
     Index <- SMDIterationPoor[,.(PriceIndex=(weighted.mean(Bundle_Value,Weight,na.rm = TRUE)/TPBV1*V1+
                       weighted.mean(MetrPrice,Weight,na.rm = TRUE)/TPBV2*V2)/V)
-        ,by=.(Region,NewArea2)]
-    Index <- Index[,.(PriceIndex=mean(PriceIndex)),by=.(Region,NewArea2)]
+        ,by=.(Region,NewArea_Name)]
+    Index <- Index[,.(PriceIndex=mean(PriceIndex)),by=.(Region,NewArea_Name)]
     
     
     SMD[,PriceIndex:=NULL]  
-    SMD <- merge(SMD,Index,by=c("Region","NewArea2"))
+    SMD <- merge(SMD,Index,by=c("Region","NewArea_Name"))
     
     SMD[,Total_Exp_Month_Per_nondurable_Real:=Total_Exp_Month_Per_nondurable/PriceIndex] 
     
@@ -156,7 +142,7 @@ for(year in (Settings$startyear:Settings$endyear)){
     SMD[,NewPoor:=ifelse(Percentile %in% Settings$InitialPoorPercentile,1,0)]
     save(SMD,file=paste0(Settings$HEISProcessedPath,"Y",year,"SMD.rda"))
     
-    
+    #cat("\n",sum(SMD[ProvinceCode==2,.N]))
     cat("\n",sum(SMD[,(ThisIterationPoor-NewPoor)^2]))
   }
   
@@ -166,101 +152,12 @@ for(year in (Settings$startyear:Settings$endyear)){
   setnames(MD,"NewPoor","InitialPoor")
   
 
-  MD[,weighted.mean(InitialPoor,Weight), by=.(NewArea2,Region)]
+  MD[,weighted.mean(InitialPoor,Weight), by=.(NewArea_Name,Region)]
   MD[,sum(Weight*Size), by=.(Decile,Region)][order(Region,Decile)]
   MD[,sum(Weight*Size), by=.(Decile_Nominal,Region)][order(Region,Decile_Nominal)]
   
   save(MD,file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoor.rda"))
   
-  ###Total
-  MDG1<-MD[,.(Population=sum(Weight*Size)), by=.(Decile,Region)][order(Region,Decile)]
-  ggplot(MDG1, aes(fill=Region, y=Population, x=Decile)) + 
-    geom_bar(position="stack", stat="identity")
-
-  ###Tehran
-  MDG2<-MD[,Tehran:=ifelse(NewArea==2301,1,0)]
-  MDG2<-MDG2[,.(Population=sum(Weight*Size)), by=.(Decile,Tehran)][order(Tehran,Decile)]
-  ggplot(MDG2, aes(fill=factor(Tehran), y=Population, x=Decile)) + 
-    geom_bar(position="stack", stat="identity")
-  
-  ###Sistan
-  MDG3<-MD[,Sistan:=ifelse(ProvinceCode==11,1,0)]
-  MDG3<-MDG3[,.(Population=sum(Weight*Size)), by=.(Decile,Sistan)][order(Sistan,Decile)]
-  ggplot(MDG3, aes(fill=factor(Sistan), y=Population, x=Decile)) + 
-    geom_bar(position="stack", stat="identity")
-  
-  
-  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"HHHouseProperties.rda"))
-  MD<-merge(MD,HHHouseProperties)
-  
-  FractioninData<-MD[,.(Total_Exp_Month_Per_nondurable=weighted.mean(Total_Exp_Month_Per_nondurable,Weight),
-                        tenure=weighted.mean(tenure=="OwnLandandBuilding",Weight),
-                        HActivityState=weighted.mean(HActivityState=="Employed",Weight),
-                        Car=weighted.mean(car=="True",Weight)),
-                     by=c("Percentile_Nominal")]
-  
-  #write.csv(FractioninData,file = "FractioninData.csv")
-  
-  Deciles<-MD[,.(HHID,Decile,Decile_Nominal,Region,ProvinceCode,HIndivNo,Weight)]
-  Deciles<-Deciles[,diff:=as.numeric(Decile)-as.numeric(Decile_Nominal)]
-  decileTest<-Deciles[,.(Positive=weighted.mean(diff>0,Weight),
-                         Zero=weighted.mean(diff==0,Weight),
-                         Minus=weighted.mean(diff<0,Weight)),by=ProvinceCode]
-  
-  decile7<-Deciles[,.(before=weighted.mean(as.numeric(Decile_Nominal)>7,Weight),
-                      after=weighted.mean(as.numeric(Decile)>7,Weight)),by=ProvinceCode]
-  
-  ###Exp
-  x2<-FractioninData[,.(Total_Exp_Month_Per_nondurable,Percentile_Nominal)]
-  x2$Percentile_Nominal <- factor(x2$Percentile_Nominal, levels = x2$Percentile_Nominal[order(x2$Percentile_Nominal)])
-  ggplot(x2, aes(x = x2$Percentile_Nominal, y = x2$Total_Exp_Month_Per_nondurable)) + theme_bw() + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1))
-  
-
-  
-  ###Tenure
-  x2<-FractioninData[,.(tenure,Percentile_Nominal)]
-  x2$Percentile_Nominal <- factor(x2$Percentile_Nominal, levels = x2$Percentile_Nominal[order(x2$tenure)])
-  ggplot(x2, aes(x = x2$Percentile_Nominal, y = x2$tenure)) + theme_bw() + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1))
-  
-
-  
-  ###HActivity
-  x2<-FractioninData[,.(HActivityState,Percentile_Nominal)]
-  x2$Percentile_Nominal <- factor(x2$Percentile_Nominal, levels = x2$Percentile_Nominal[order(x2$HActivityState)])
-  ggplot(x2, aes(x = x2$Percentile_Nominal, y = x2$HActivityState)) + theme_bw() + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1))
-  
- 
-  ###Car
-  x2<-FractioninData[,.(Car,Percentile_Nominal)]
-  x2$Percentile_Nominal <- factor(x2$Percentile_Nominal, levels = x2$Percentile_Nominal[order(x2$Car)])
-  ggplot(x2, aes(x = x2$Percentile_Nominal, y = x2$Car)) + theme_bw() + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1))
-  
-  decile7<-Deciles[,.(before=weighted.mean(as.numeric(Decile_Nominal)>7,Weight),
-                      after=weighted.mean(as.numeric(Decile)>7,Weight)),by=ProvinceCode]
-  
-  Deciles<-MD[,.(HHID,Decile,Decile_Nominal,Region,ProvinceCode,HIndivNo,Weight,
-                 Total_Exp_Month,Total_Exp_Month_nondurable)]
-  Deciles<-Deciles[,diff:=as.numeric(Decile)-as.numeric(Decile_Nominal)]
-  decileTest<-Deciles[,.(Positive=weighted.mean(diff>0,Weight),
-                         Zero=weighted.mean(diff==0,Weight),
-                         Minus=weighted.mean(diff<0,Weight)),by=ProvinceCode]
-  
-  SMD[,TIncome:=sum(Total_Exp_Month_Per_nondurable*Weight)]
-  Share<-SMD[,Income_Share:=Total_Exp_Month_Per_nondurable/TIncome]
-  Share<-Share[,.(Income_Share=sum(Income_Share*Weight)),by=Decile]
-  save(Share,file = "Share.rda")
-  
-  Deciles<-Deciles[,Low:=ifelse(as.numeric(Decile)<4,1,0)]
-  Low<-Deciles[,.(weighted.mean(Low,Weight)),by=ProvinceCode]
-  save(Low,file = "Low.rda")
-  
-  
-  Deciles<-Deciles[,L5:=ifelse(Total_Exp_Month_nondurable<5000000,1,0)]
-  L5<-Deciles[,.(sum(L5*Weight)),by=ProvinceCode]
-  save(L5,file = "L5.rda")
-  
-  #UnEmp<-MD[,UnEmp:=ifelse(HActivityState=="Unemployed" & HAge<41,1,0)]
-  #UnEmp<-UnEmp[,.(weighted.mean(UnEmp,Weight)),by=ProvinceCode]
 }
 
 
