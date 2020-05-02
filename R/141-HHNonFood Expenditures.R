aa778786b86cb2c11fa643ebf9b8e179f3ba9318
Arin Shahbazian <arin1989@gmail.com>
2020-05-02 23:55:13 +0430

    Add new code files and edits for new poverty calculations

--- R/141-HHNonFood Expenditures.R
+++ R/141-HHNonFood Expenditures.R
@@ -518,16 +518,16 @@ for(year in (Settings$startyear:Settings$endyear)){
   TC <- TC[,pcols,with=FALSE]
 
   if(year %in% 84:97){
-    TC[,Durable_Exp:=as.numeric(Durable_Exp)]
-   # TC[,Durable_Sale:=as.numeric(Durable_Sale)]
+    TC[,Durable_Exp:=as.numeric(Durable_Exp)/12]
+   TC[,Durable_Sale:=as.numeric(Durable_Sale)/12]
   }
-  TC<-TC[BuyingMethod==1]
-  TC$Durable_Exp<-TC$Durable_Exp/12
-  #TC$Durable_Sale<-TC$Durable_Sale/12
+  #TC<-TC[BuyingMethod==1]
+  TC$Durable_Exp<-TC$Durable_Exp
+  TC$Durable_Sale<-TC$Durable_Sale
   TC[,Code:=NULL]
   TC[,BuyingMethod:=NULL]
   TC[is.na(TC)] <- 0
-  #TC[,Durable_Pure_Exp:=Durable_Exp-Durable_Sale]
+  TC[,Durable_Pure_Exp:=Durable_Exp-Durable_Sale]
   DurableData <- TC[,lapply(.SD,sum),by=HHID]
   save(DurableData, file = paste0(Settings$HEISProcessedPath,"Y",year,"Durables.rda"))
 }
