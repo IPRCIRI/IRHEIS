e8327a97ae2090f75c943e907f5b4f75d4d3556a
Arin Shahbazian <arin1989@gmail.com>
2020-05-02 23:55:13 +0430

    Add new code files and edits for new poverty calculations

---
+++ R/161-Step1-MergeData.R
@@ -0,0 +1,153 @@
+#171-Step 1.R
+# 
+# Copyright © 2018: Majid Einian & Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ Merge Data =====================================\n")
+
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(readxl)
+library(stringr)
+library(data.table)
+library(ggplot2)
+library(spatstat)
+
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  
+  #load Demos+FoodPrices+Weights
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"HHBase.rda"))
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"lactating.rda"))
+  HHBase<-merge(HHBase,lactating,by="HHID")
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"Calorie_Need.rda"))
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"HHI.rda"))
+  #load(file=paste0(Settings$HEISProcessedPath,"Y",year,"BigFoodPrice.rda"))
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"Value.rda"))
+  
+  load(file=paste0(Settings$HEISWeightsPath,Settings$HEISWeightFileName,year,".rda"))
+  HHWeights<- as.data.table(HHWeights)
+  HHWeights<-HHWeights[,HHID:=as.numeric(HHID)]
+  HHWeights[,Year:=NULL]
+  
+  Value[,Auto2_rani:=0.1*Auto2_rani]
+  Value[,Auto1_Khareji:=0.1*Auto1_Khareji]
+  Value[,Auto2_Khareji:=0.1*Auto2_Khareji]
+  Value[,Auto1_Irani:=0.1*Auto1_Irani]
+  Value[,TV_Rangi_Irani:=0.033*TV_Rangi_Irani]
+  Value[,TV_Rangi_Khareji:=0.033*TV_Rangi_Khareji]
+  Value[,freezer2:=0.033*freezer2]
+  Value[,OjaghGaz:=0.033*OjaghGaz]
+  Value[,Mashin_Lebasshooyi:=0.033*Mashin_Lebasshooyi]
+  Value[,Mobile:=0.11*Mobile]
+  Value[,Cooler_Gaz:=0.05*Cooler_Gaz]
+  Value[,PC:=0.06*PC]
+  Value[,Lastik_Mashin:=0.5*Lastik_Mashin]
+  Value[,Motor_Machin:=0.033*Motor_Machin]
+  Value[,Tamirat_Asasi:=0.05*Tamirat_Asasi]
+  
+  dep <- c( "Auto2_rani", "Auto1_Khareji","Auto2_Khareji", "Auto1_Irani",
+           "TV_Rangi_Irani", "TV_Rangi_Khareji","freezer2", "OjaghGaz",
+           "Mashin_Lebasshooyi", "Mobile","Cooler_Gaz", "PC",
+           "Lastik_Mashin", "Motor_Machin","Tamirat_Asasi")
+  
+  Value[, Total_Depreciated_Durable := Reduce(`+`, .SD), .SDcols=dep]
+  
+  
+  #load Expenditures
+  
+  for(G in c("Foods","Cigars","Cloths","Amusements","Communications",
+             "Durables", "Education", "Furnitures","HotelRestaurants",
+             "HouseandEnergys","House", "Medicals","Hygienes","Transportations","Others",
+             "Resturants"
+             ,"Durablele_Detail"
+  )){
+    load(file=paste0(Settings$HEISProcessedPath,"Y",year,G,".rda"))
+    load(file = paste0(Settings$HEISProcessedPath,"Y",year,"NonFreeDurableData.rda"))
+    
+  }
+  
+  # load(file=paste0(Settings$HEISProcessedPath,"Y",year,"Added_Food.rda")) 
+  
+  #load Calories
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"Food_Calories.rda"))
+  FData[,Region:=NULL]
+  #for (col in c("FoodKCaloriesHH")) FData[is.na(get(col)), (col) := 0]
+  FData <- FData[FoodKCaloriesHH>0]
+  
+  #merge groups
+  MD<-merge(HHBase,HHI ,by =c("HHID"),all=TRUE)
+  FData[,Size:=NULL]
+  MD<-merge(MD,FData ,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,HHWeights ,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,FoodData,by =c("HHID"),all=TRUE)
+  # MD<-merge(MD,Added_Food,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,CigarData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,ClothData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,AmusementData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,CommunicationData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,EducData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,HouseandEnergyData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,HouseData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,FurnitureData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,HotelRestaurantData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,HygieneData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,TransportationData,by =c("HHID"),all=TRUE)
+  #MD<-merge(MD,BenzinData,by =c("HHID"),all=TRUE)
+  #MD<-merge(MD,GazData,by =c("HHID"),all=TRUE)
+  #MD<-merge(MD,BarghData,by =c("HHID"),all=TRUE)
+  #MD<-merge(MD,NaftSefidData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,OtherData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,MedicalData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,NonFreeDurableData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,ResturantData,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,Durablele_Detail,by =c("HHID"),all=TRUE)
+  MD<-merge(MD,Value,by =c("HHID"),all=TRUE)
+  #MD<-merge(MD,InvestmentData,by =c("HHID"),all=TRUE)
+  for (col in c("OriginalFoodExpenditure","FoodOtherExpenditure", "Cigar_Exp", "Cloth_Exp", "Amusement_Exp", 
+                "Communication_Exp", "Education_Exp", "HouseandEnergy_Exp", 
+                "Furniture_Exp", "HotelRestaurant_Exp", "Hygiene_Exp", "Transportation_Exp",
+                "Other_Exp", "Medical_Exp", "NonFreeDurable_Exp",
+                "Resturant_Exp","ServiceExp"
+                ,"Add_to_NonDurable","Durable_Dep",
+                "Durable_NoDep","Durable_Emergency","Total_Depreciated_Durable"
+  )) 
+    MD[is.na(get(col)), (col) := 0]
+  #  MD<-MD[,Yaraneh:=416000*Size]
+  
+  MD<-merge(MD,Calorie_Need)
+  #load(file=paste0(Settings$HEISProcessedPath,"Y",year,"Durablele_Detail.rda"))
+  #MD<-merge(MD,Durablele_Detail)
+  
+  #Calculate Monthly Total Expenditures 
+  nw <- c("OriginalFoodExpenditure","FoodOtherExpenditure", "Cigar_Exp", "Cloth_Exp",
+          "Amusement_Exp", "Communication_Exp", 
+          "HouseandEnergy_Exp", "Furniture_Exp", "HotelRestaurant_Exp", "Hygiene_Exp", 
+          "Transportation_Exp", "Other_Exp"
+          #,"Add_to_NonDurable","Total_Depreciated_Durable"
+  )
+  w <- c(nw, "Medical_Exp",
+         "Durable_NoDep","Durable_Emergency")
+ # w <- c(nw, "Medical_Exp", "NonFreeDurable_Exp")
+  #w <- c(nw, "Medical_Exp", "Durable_Pure_Exp")
+  # pw <- c(nw, "Added_Food_Exp_Month")
+  #Lw <- c(pw,  "Medical_Exp", "Durable_Exp")
+  
+  MD[, Total_Exp_Month := Reduce(`+`, .SD), .SDcols=w]
+  MD[, Total_Exp_Month_nondurable := Reduce(`+`, .SD), .SDcols=nw]
+  
+  MD[,weighted.mean(Total_Exp_Month,Weight)]
+  MD[,weighted.mean(Total_Exp_Month_nondurable,Weight)]
+  
+  save(MD, file=paste0(Settings$HEISProcessedPath,"Y",year,"Merged4CBN1.rda"))
+}
+
+
+
+endtime <- proc.time()
+cat("\n\n============================\nIt took ")
+cat(endtime-starttime)
---
+++ R/165-Step5-AddClusterInfo.R
@@ -0,0 +1,117 @@
+#183-Clustering(93-96).R
+# 
+# Copyright © 2018: Majid Einian & Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ Prepare Data =====================================\n")
+library(yaml)
+library(dplyr)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(readxl)
+library(data.table)
+library(ggplot2)
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoor.rda"))
+  
+  #####Clustering#####
+  dt2Urban<-MD[Region=="Urban",.(NewArea,NewArea_Name,Region,HHID)]
+  dt2Rural<-MD[Region=="Rural",.(NewArea,NewArea_Name,Region,HHID)]
+  
+  #####Urban#####
+  dt2Urban<-dt2Urban[NewArea_Name=="Sh_Tehran",cluster3:=1]
+  
+  dt2Urban<-dt2Urban[NewArea_Name=="Sh_Shiraz" | NewArea_Name=="Sh_Esfahan" |
+                       NewArea_Name=="Sh_Bandarabas" |
+                       NewArea_Name=="Sh_Karaj" |
+                       NewArea_Name=="Sh_Rasht",
+                     cluster3:=2]
+  
+  dt2Urban<-dt2Urban[NewArea_Name=="Gilan" | NewArea_Name=="Alborz" |
+                       NewArea_Name=="Kohkilooye" | NewArea_Name=="Ghazvin" | 
+                       NewArea_Name=="Markazi"   |  NewArea_Name=="Esfahan" |
+                       NewArea_Name=="Ghom" |  NewArea_Name=="Sh_Arak" |
+                       NewArea_Name=="Tehran" |  NewArea_Name=="Sh_Tabriz" |
+                       NewArea_Name=="Mazandaran"  | NewArea_Name=="Sh_Yazd" ,
+                     cluster3:=3]
+  
+  dt2Urban<-dt2Urban[  NewArea_Name=="Yazd" |
+                         NewArea_Name=="Sh_Kerman" |
+                         NewArea_Name=="Sh_Hamedan" |
+                         NewArea_Name=="Chaharmahal" |
+                         NewArea_Name=="Zanjan" | NewArea_Name=="Hamedan" |
+                         NewArea_Name=="Sh_Mashhad" |  NewArea_Name=="Sh_Urmia" |
+                         NewArea_Name=="Sh_Ahvaz" |   NewArea_Name=="Hormozgan"|
+                         NewArea_Name=="Booshehr" |
+                         NewArea_Name=="Semnan" | NewArea_Name=="Fars",
+                       cluster3:=4]
+  
+  dt2Urban<-dt2Urban[ NewArea_Name=="Sh_Kermanshah" | NewArea_Name== "Az_Sharghi" |
+                        NewArea_Name=="Kerman" | 
+                        NewArea_Name=="Ardebil" |
+                        NewArea_Name=="Ilam" ,
+                      cluster3:=5]
+  
+  dt2Urban<-dt2Urban[NewArea_Name=="Khoozestan" | NewArea_Name=="Kordestan" |
+                       NewArea_Name=="Khorasan_Razavi" | 
+                       NewArea_Name=="Khorasan_Jonoobi" | NewArea_Name=="Az_Gharbi" |
+                       NewArea_Name=="Kermanshah" | NewArea_Name=="Lorestan" |
+                       NewArea_Name=="Khorasan_Shomali" | NewArea_Name=="Golestan",
+                     cluster3:=6]
+  
+  dt2Urban<-dt2Urban[NewArea_Name=="Sistan" | NewArea_Name=="Sh_Zahedan",
+                     cluster3:=7]
+  
+  save(dt2Urban,file ="dt2Urban.rda")
+  #####Rural#####
+  dt2Rural<-dt2Rural[ NewArea_Name=="Tehran" | NewArea_Name=="Alborz",
+                      cluster3:=8]
+  
+  dt2Rural<-dt2Rural[NewArea_Name=="Esfahan" | NewArea_Name=="Mazandaran" ,
+                     cluster3:=9]
+  
+  dt2Rural<-dt2Rural[ NewArea_Name=="Ghazvin" | NewArea_Name=="Yazd"  |
+                        NewArea_Name=="Gilan" |
+                        NewArea_Name=="Kohkilooye" | NewArea_Name=="Booshehr",
+                      cluster3:=10]
+  
+  dt2Rural<-dt2Rural[ NewArea_Name=="Zanjan" |
+                        NewArea_Name=="Chaharmahal"| NewArea_Name=="Hormozgan" | NewArea_Name=="Az_Sharghi" | 
+                        NewArea_Name=="Ghom" | 
+                        NewArea_Name=="Fars" |  
+                        NewArea_Name=="Markazi" |  NewArea_Name=="Semnan",
+                      cluster3:=11]
+  
+  dt2Rural<-dt2Rural[  NewArea_Name=="Hamedan" |
+                         NewArea_Name=="Az_Gharbi" |  NewArea_Name=="Kermanshah" |
+                         NewArea_Name=="Kerman" | NewArea_Name=="Golestan" |
+                         NewArea_Name=="Khorasan_Jonoobi" | NewArea_Name=="Lorestan" |
+                         NewArea_Name=="Kordestan"|
+                         NewArea_Name=="Khoozestan" |
+                         NewArea_Name=="Ardebil" | NewArea_Name=="Ilam",
+                       cluster3:=12]
+  
+  dt2Rural<-dt2Rural[ NewArea_Name=="Khorasan_Shomali" | NewArea_Name=="Khorasan_Razavi" |
+                        NewArea_Name=="Sistan" ,
+                      cluster3:=13]
+  
+  save(dt2Rural,file ="dt2Rural.rda")
+  #####Merge#####
+  dt2total<-rbind(dt2Urban,dt2Rural)
+  
+  
+  dt2total[,HHID:=NULL]
+  dt2total<-distinct(dt2total)
+  
+  MD<-merge(MD,dt2total,by=c("NewArea","NewArea_Name","Region"))
+  save(MD,file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoorClustered.rda"))
+}
+
+endtime <- proc.time()
+cat("\n\n============================\nIt took ")
+cat((endtime-starttime)["elapsed"])
+cat(" seconds")
