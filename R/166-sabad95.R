8149e92437265ce3691400ac961ab25648582567
zahrashahidi <51110342+zahrashahidi@users.noreply.github.com>
2020-07-25 19:43:31 +0430

    Updating sabad95.R

--- R/166-sabad95.R
+++ R/166-sabad95.R
@@ -17,7 +17,81 @@ library(stringr)
 library(readxl)
 library(spatstat)
 library(writexl)
+
 year<-95
+
+load( file = paste0(Settings$HEISProcessedPath,"Y",year,"BigFData.rda"))
+#load(file = paste0(Settings$HEISProcessedPath,"Y",year,"Food_Calories.rda"))
+
+goosht<-BigFData[FoodType=="Goosht"]
+goosht<-goosht[,Goosht_Grams:=FGrams ]
+morgh<-BigFData[FoodType=="Morgh"]
+morgh<-morgh[,Morgh_Grams:=FGrams ]
+mive<-BigFData[FoodType=="Mive"]
+mive<-mive[,Mive_Grams:=FGrams ]
+nan<-BigFData[FoodType=="Nan"]
+nan<-nan[,Nan_Grams:=FGrams ]
+sibzamini<-BigFData[FoodType=="Sibzamini"]
+sibzamini<-sibzamini[,Sibzamini_Grams:=FGrams ]
+makarooni<-BigFData[FoodType=="Makarooni"]
+makarooni<-makarooni[,Makarooni_Grams:=FGrams ]
+berenj<-BigFData[FoodType=="Berenj"]
+berenj<-berenj[,Berenj_Grams:=FGrams ]
+hoboobat<-BigFData[FoodType=="Hoboobat"]
+hoboobat<-hoboobat[,Hoboobat_Grams:=FGrams ]
+sabzi<-BigFData[FoodType=="Sabzi"]
+sabzi<-sabzi[,Sabzi_Grams:=FGrams ]
+roghan<-BigFData[FoodType=="Roghan"]
+roghan<-roghan[,Roghan_Grams:=FGrams ]
+ghand<-BigFData[FoodType=="Ghand"]
+ghand<-ghand[,Ghand_Grams:=FGrams ]
+shir<-BigFData[FoodType=="Shir"]
+shir<-shir[,Shir_Grams:=FGrams]
+panir<-BigFData[FoodType=="Panir"]
+panir<-shir[,Panir_Grams:=FGrams]
+mast<-BigFData[FoodType=="Mast"]
+mast<-mast[,Mast_Grams:=FGrams]
+tokhmemorgh<-BigFData[FoodType=="Tokhmemorgh"]
+tokhmemorgh<-tokhmemorgh[,Tokhmemorgh_Grams:=FGrams ]
+
+load(file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoorClustered.rda"))
+
+MD<-merge(MD,goosht[,.(HHID,Goosht_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,morgh[,.(HHID,Morgh_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,mive[,.(HHID,Mive_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,nan[,.(HHID,Nan_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,sibzamini[,.(HHID,Sibzamini_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,makarooni[,.(HHID,Makarooni_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,berenj[,.(HHID,Berenj_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,hoboobat[,.(HHID,Hoboobat_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,sabzi[,.(HHID,Sabzi_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,roghan[,.(HHID,Roghan_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,ghand[,.(HHID,Ghand_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,shir[,.(HHID,Shir_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,panir[,.(HHID,Panir_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,mast[,.(HHID,Mast_Grams)],by=c("HHID"),all.x = T)
+MD<-merge(MD,tokhmemorgh[,.(HHID,Tokhmemorgh_Grams)],by=c("HHID"),all.x = T)
+MD<-MD[,Labaniat_Grams:=Shir_Grams+Panir_Grams+Mast_Grams]
+MD[,Selected_Group:=ifelse((Region=="Urban" & Decile==3) |
+                             (Region=="Rural" & Decile==2),1,0)]
+
+gram <-MD [Selected_Group==1,
+           .(Nan_Grams= weighted.mean(Nan_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Berenj_Grams= weighted.mean(Berenj_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Makarooni_Grams=weighted.mean(Makarooni_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Hoboobat_Grams=weighted.mean(Hoboobat_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Sibzamini_Grams=weighted.mean(Sibzamini_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Sabzi_Grams=weighted.mean(Sabzi_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Mive_Grams=weighted.mean(Mive_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Goosht_Grams=weighted.mean(Goosht_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Morgh_Grams=weighted.mean(Morgh_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Tokhmemorgh_Grams=weighted.mean(Tokhmemorgh_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Labaniat_Grams=weighted.mean(Labaniat_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Roghan_Grams=weighted.mean(Roghan_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             Ghand_Grams=weighted.mean(Ghand_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
+             number=.N)]
+
+
 for(year in (Settings$startyear:Settings$endyear)){
   cat(paste0("\n------------------------------\nYear:",year,"\n"))
   
@@ -88,24 +162,7 @@ for(year in (Settings$startyear:Settings$endyear)){
                  number=.N),
                by=.(cluster3,Region)]
     
-    #by=.(cluster3,Region)]
-    gram <-MD [Selected_Group==1,
-               .(Nan_Grams= weighted.mean(Nan_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Berenj_Grams= weighted.mean(Berenj_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Makarooni_Grams=weighted.mean(Makarooni_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Hoboobat_Grams=weighted.mean(Hoboobat_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Sibzamini_Grams=weighted.mean(Sibzamini_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Sabzi_Grams=weighted.mean(Sabzi_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Mive_Grams=weighted.mean(Mive_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Goosht_Grams=weighted.mean(Goosht_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Morgh_Grams=weighted.mean(Morgh_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Tokhmemorgh_Grams=weighted.mean(Tokhmemorgh_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Labaniat_Grams=weighted.mean(Labaniat_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Roghan_Grams=weighted.mean(Roghan_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 Ghand_Grams=weighted.mean(Ghand_Grams/EqSizeCalory,Weight*Size,na.rm=TRUE),
-                 number=.N),
-               by=.(cluster3,Region)]
-    
+     
     MDP[is.na(MDP)] <- 0
     min<-MDP[FPLine>0,min(FPLine)]
     MDP[,FPLine:=ifelse(FPLine==0,min,FPLine)]
