f685ff36b12852493fead850308dcb2dcaf92d5e
Arin Shahbazian <arin1989@gmail.com>
2020-08-04 17:58:19 +0430

    Update realizing section

--- R/164-Step4-RealDeciles.R
+++ R/164-Step4-RealDeciles.R
@@ -117,35 +117,36 @@ DoDeciling_SetInitialPoor <- function(DataTable,PriceIndexDT){
   }
   DataTable<-merge(DataTable,A11,by="Decile")
   
-  DataTable[car=="True",Added1:=A1] ### We use 0.05 instead of 0.1
-  DataTable[tvcr=="True",Added2:=A2]
-  DataTable[freezer=="True" | frez_refrig=="True" | refrigerator=="True",
-        Added3:=A3]
-  DataTable[oven=="True",Added4:=A4]
-  DataTable[washer=="True",Added5:=A5]
-  DataTable[cellphone=="True",Added6:=A6]
-  DataTable[cooler_gas=="True",Added7:=A7]
-  DataTable[computer=="True",Added8:=A8]
-  DataTable[car=="True",Added9:=A9]
+#  DataTable[car=="True",Added1:=A1] ### We use 0.05 instead of 0.1
+#  DataTable[tvcr=="True",Added2:=A2]
+#  DataTable[freezer=="True" | frez_refrig=="True" | refrigerator=="True",
+#        Added3:=A3]
+#  DataTable[oven=="True",Added4:=A4]
+#  DataTable[washer=="True",Added5:=A5]
+#  DataTable[cellphone=="True",Added6:=A6]
+#  DataTable[cooler_gas=="True",Added7:=A7]
+#  DataTable[computer=="True",Added8:=A8]
+ # DataTable[car=="True",Added9:=A9]
+#  if (year!=90 & year!=92 & year!=93 & year!=95){
+ #   DataTable[car=="True",Added10:=A10]
+ # }
+ # DataTable[car=="True",Added11:=A11]
+  
+  DataTable[car=="True",Added1:=A1-0.05*Auto_Sale]  #We use 0.05 instead of 0.1
+    DataTable[tvcr=="True",Added2:=A2-0.033*TV_Sale]
+      DataTable[freezer=="True" | frez_refrig=="True" | refrigerator=="True",
+              Added3:=A3-0.033*yakhchal_Sale]
+    DataTable[oven=="True",Added4:=A4-0.033*ojaghgaz_Sale]
+    DataTable[washer=="True",Added5:=A5-0.033*lebasshooyi_Sale]
+    DataTable[cellphone=="True",Added6:=A6-0.11*Mobile_Sale]
+    DataTable[cooler_gas=="True",Added7:=A7-0.05*Coolergazi_Sale]
+      DataTable[computer=="True",Added8:=A8]
+    DataTable[computer=="True",Added8:=A8-0.06*PC_Sale]
+  DataTable[car=="True",Added9:=A9-0.5*lastik_Sale]
   if (year!=90 & year!=92 & year!=93 & year!=95){
-    DataTable[car=="True",Added10:=A10]
+   DataTable[car=="True",Added10:=A10]
   }
   DataTable[car=="True",Added11:=A11]
-  
-#  DataTable[car=="True",Added1:=A1-0.05*Auto_Sale] ### We use 0.05 instead of 0.1
-  #  DataTable[tvcr=="True",Added2:=A2-0.033*TV_Sale]
-    #  DataTable[freezer=="True" | frez_refrig=="True" | refrigerator=="True",
-  #            Added3:=A3-0.033*yakhchal_Sale]
-  #  DataTable[oven=="True",Added4:=A4-0.033*ojaghgaz_Sale]
-  #  DataTable[washer=="True",Added5:=A5-0.033*lebasshooyi_Sale]
-  #  DataTable[cellphone=="True",Added6:=A6-0.11*Mobile_Sale]
-  #  DataTable[cooler_gas=="True",Added7:=A7-0.05*Coolergazi_Sale]
-  #  DataTable[computer=="True",Added8:=A8-0.06*PC_Sale]
-  #DataTable[car=="True",Added9:=A9-0.5*lastik_Sale]
-  #if (year!=90 & year!=92 & year!=93 & year!=95){
-  # DataTable[car=="True",Added10:=A10]
-  #}
-  #DataTable[car=="True",Added11:=A11]
   
   if (year!=90 & year!=92 & year!=93 & year!=95){ 
   dep <- c( "71111", "71112","71116", "71117",
@@ -288,8 +289,8 @@ for(year in (Settings$startyear:Settings$endyear)){
 
   
   #comparison <- compare(S1,S2,"row")
-  comparison <- setdiff(S1$HHID,S2$HHID)
-  x<-S1[HHID %in% comparison]
+  #comparison <- setdiff(S1$HHID,S2$HHID)
+  #x<-S1[HHID %in% comparison]
   
   SMD[,InitialPoorBasedOnPercentileLastIteration:=1]
   i <- 0
@@ -303,6 +304,8 @@ for(year in (Settings$startyear:Settings$endyear)){
       stop("HERE Some Area goes missing!")
     if(min(SMDIterationPoor[,.N,by=.(Region,NewArea_Name)]$N)==0)
       stop("HERE Some Area goes missing!")
+    
+    
     
     PriceDTBasedOnThisIterationPoor <- CalcTornqvistIndex(SMDIterationPoor)
     #   print(PriceDTBasedOnThisIterationPoor[Region=="Rural" & NewArea_Name=="Semnan",])
@@ -318,7 +321,31 @@ for(year in (Settings$startyear:Settings$endyear)){
 #  cat(SMD[,weighted.mean(TFoodKCaloriesHH_Per,Weight)],"\n")
 #  cat(SMD[,weighted.mean(Bundle_Value,Weight)],"\n")
   
-  MD <- merge(MD,SMD[,.(HHID,Bundle_Value,InitialPoorBasedOnPercentile,Decile,Percentile)],by="HHID")
+  MD[,Added1:=NULL]
+  MD[,Added2:=NULL]
+  MD[,Added3:=NULL]
+  MD[,Added4:=NULL]
+  MD[,Added5:=NULL]
+  MD[,Added6:=NULL]
+  MD[,Added7:=NULL]
+  MD[,Added8:=NULL]
+  MD[,Added9:=NULL]
+  MD[,Added10:=NULL]
+  MD[,Added11:=NULL]
+  MD[,Added:=NULL]
+  
+  if (year!=90 & year!=92 & year!=93 & year!=95){ 
+  MD <- merge(MD,SMD[,.(HHID,Bundle_Value,InitialPoorBasedOnPercentile,Decile,Percentile,
+                        Added1,Added2,Added3,Added4,Added5,Added6,Added7,Added8,
+                        Added9,Added10,Added11,Added)],by="HHID")
+  }
+  
+  if (year==90 | year==92 | year==93 | year==95){
+    MD <- merge(MD,SMD[,.(HHID,Bundle_Value,InitialPoorBasedOnPercentile,Decile,Percentile,
+                          Added1,Added2,Added3,Added4,Added5,Added6,Added7,Added8,
+                          Added9,Added11,Added)],by="HHID")
+  }
+  
   setnames(MD,"InitialPoorBasedOnPercentile","InitialPoor")  # or maybe InitialPoorBasedOnRealIterativePercentile !
   
 
@@ -340,6 +367,9 @@ save(MD,file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoor.rda"))
 }
 
 x<-MD[,weighted.mean(FoodKCaloriesHH_Per,Weight),by=c("Region,Decile")]
+
+#z2<-MD[,.(HHID,Decile,Added,Added1,Added2,Added3,Added4,Added5,
+ #         Added6,Added7,Added8,computer,Added9,Added10,Added11)]
 
 endtime <- proc.time()
 cat("\n\n============================\nIt took ")
