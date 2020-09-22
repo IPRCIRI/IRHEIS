5cecde54f1d02cf44fd5deef51eee062592f5a3d
Majid Einian <einian85@users.noreply.github.com>
2020-09-22 20:25:55 +0330

    tidy up code

--- R/132-HHNonFood Expenditures.R
+++ R/132-HHNonFood Expenditures.R
@@ -7,7 +7,7 @@
 rm(list=ls())
 
 startTime <- proc.time()
-cat("\n\n================ NonFood Expenditures =====================================\n")
+cat("\n\n================ NonFood Expenditures =============================\n")
 
 library(yaml)
 Settings <- yaml.load_file("Settings.yaml")
@@ -20,11 +20,12 @@ sections_names <- c("Cigar","Cloth","Communication","Energy","Furniture",
                     "Hygiene","Medical","Transportation","Communication",
                     "Amusement","Education","Hotel","Restaurant","Other",
                     "Investment")
-# House has different code
+# House has different code / durables are in another file
 for(section in sections_names){
   section_sheet <- eval(parse(text = paste0("Settings$MDS_",section)))
   cat("\n\n================",section,"=====================================\n")
-  SectionTables <- data.table(read_excel(Settings$MetaDataFilePath,sheet=section_sheet))
+  SectionTables <- data.table(read_excel(Settings$MetaDataFilePath,
+                                         sheet=section_sheet))
   
   for(year in (Settings$startyear:Settings$endyear)){
     cat(paste0("\n------------------------------\nYear:",year,"\n"))
@@ -48,26 +49,27 @@ for(section in sections_names){
     }
     
     TS[,(paste0(section,"_Exp")):=as.numeric(get(paste0(section,"_Exp")))]
-    
     TS[,Code:=NULL]
     TS[is.na(TS)] <- 0
     
     eval(parse(text = paste0(section,"Data <- TS[,lapply(.SD,sum),by=HHID]")))
-    eval(parse(text = paste0("save(",section,"Data, file =paste0(Settings$HEISProcessedPath,\"Y\",year,\"",section,"s.rda\"))")))
-    eval(parse(text = paste0("cat(section,\":\",",section,"Data[,mean(",section,"_Exp)])")))
+    eval(parse(text = paste0("save(",section,
+                             "Data, file =paste0(Settings$HEISProcessedPath,\"Y\",year,\"",
+                             section,"s.rda\"))")))
+    eval(parse(text = paste0("cat(section,\":\",",section,
+                             "Data[,mean(",section,"_Exp)])")))
   }
 }
 
 
 cat("\n\n================ Section4:HHHouse =====================================\n")
-
 HouseTables <- data.table(read_excel(Settings$MetaDataFilePath,sheet=Settings$MDS_House))
-#p0<-0
+
 for(year in (Settings$startyear:Settings$endyear)){
   cat(paste0("\n------------------------------\nYear:",year,"\t"))
   
   load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
-  
+
   ty <- HouseTables[Year==year][,1:8]
   tab <- ty$Table
   
@@ -83,23 +85,20 @@ for(year in (Settings$startyear:Settings$endyear)){
   TL <- TL[,pcols,with=FALSE]
   TL <- TL[Code %in% ty$StartCode:ty$EndCode]
   TL[,House_Exp:=as.numeric(House_Exp)]
-  
   TL[,Code:=NULL]
   TL[is.na(TL)] <- 0
   
   HouseData <- TL[,lapply(.SD,sum),by=HHID]
-  
-  
+
   load(file=paste0(Settings$HEISProcessedPath,"Y",year,
                    "HHHouseProperties.rda"))
   HousePropData <- HHHouseProperties[,.(HHID,room,area)]
-  # cat("\n",year,",",mean(HouseData$ServiceExp,na.rm = TRUE))
-  
+
   HouseData <- merge(HouseData,HousePropData,by = "HHID", all = TRUE)
   HouseData <- HouseData[,MeterPrice:=House_Exp/area]
   HouseData <- HouseData[,.(HHID,House_Exp,MeterPrice)]
-  save(HouseData, file = paste0(Settings$HEISProcessedPath,"Y",year,"House.rda"))
+  save(HouseData, file=paste0(Settings$HEISProcessedPath,"Y",year,"House.rda"))
 }
 
 endTime <- proc.time()
-cat("\n\n============================\nIt took",(endTime-startTime)[3], "seconds.")
+cat("\n\n=========================\nIt took",(endTime-startTime)[3], "seconds.")
