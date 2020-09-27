209d6cca6132fbfca59ade77dbef72ffab707451
Majid Einian <einian85@users.noreply.github.com>
2020-09-27 21:20:44 +0330

    Process Price data too

--- R/151-Food Groups for Calorie.R
+++ R/151-Food Groups for Calorie.R
@@ -1,7 +1,7 @@
-# 151-Food Groups for calorie.R
+# 151-Food Groups for calorie.R ---- and for prices
 # Builds the Food Groups data.table for households
 #
-# Copyright © 2018: Arin Shahbazian
+# Copyright © 2018-2020: Arin Shahbazian & Majid Einian
 # Licence: GPL-3
 
 rm(list=ls())
@@ -15,16 +15,24 @@ library(readxl)
 
 cat("\n\n================ FoodGroups =====================================\n")
 TFoodGroups <- data.table(read_excel(Settings$MetaDataFilePath,Settings$MDS_FoodGroups))
+FoodTypeTables <- list()
+for(i in 1:nrow(TFoodGroups))
+  FoodTypeTables[[i]] <- data.table(read_excel(Settings$MetaDataFilePath,sheet=TFoodGroups[i,SheetName]))
+
 for(year in (Settings$startyear:Settings$endyear)){
   cat(paste0("\n------------------------------\nYear:",year,"\n"))
-  BigFData <- data.table(HHID=NA_integer_,FGrams=NA_real_,Expenditure=NA_real_,
-                         FoodType=NA_character_,FoodKCalories=NA_real_,
+  
+  BigFData <- data.table(HHID=NA_integer_,FoodType=NA_character_,Price=NA_real_,
+                         Expenditure=NA_real_,FGrams=NA_real_,
+                         FoodKCalories=NA_real_,
                          FoodProtein=NA_real_)[0]
+  
+  load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
+  
   for(i in 1:nrow(TFoodGroups)){
     cat(paste0(TFoodGroups[i,SheetName],", "),"\t")
     
-    ThisFoodTypeTable <- data.table(read_excel(Settings$MetaDataFilePath,sheet=TFoodGroups[i,SheetName]))
-    load(file=paste0(Settings$HEISRawPath,"Y",year,"Raw.rda"))
+    ThisFoodTypeTable <- FoodTypeTables[[i]] 
     ft <- ThisFoodTypeTable[Year==year]
     tab <- ft$Table
     if(is.na(tab))
@@ -38,42 +46,48 @@ for(year in (Settings$startyear:Settings$endyear)){
         setnames(TF,n,names(ft)[x])
     }
     if(year %in% 63:82){
-      pcols <- intersect(names(TF),c("HHID","Code","Kilos","Expenditure"))
+      pcols <- intersect(names(TF),c("HHID","Code","Kilos","Expenditure","Price"))
       TF <- TF[,pcols,with=FALSE]
       TF <- TF[Code %in% ft$StartCode:ft$EndCode]
+      TF <- TF[!(is.na(Kilos) & is.na(Expenditure) & is.na(Price))] 
       
       TF[,Kilos:=as.numeric(Kilos)]
   
-      TF[,Code:=NULL]
-      TF[is.na(TF)] <- 0
-      TF[,FGrams:=(Kilos*1000)/30]
-    }
-    if(year >= 83){
-      pcols <- intersect(names(TF),c("HHID","Code","Grams","Kilos","Expenditure"))
-     # TF <- TF[,pcols,with=FALSE]
+      TF[,FGrams:=(Kilos*1000)]
+      
+    } else if(year >= 83){
+      pcols <- intersect(names(TF),c("HHID","Code","Grams","Kilos","Expenditure","Price"))
+      TF <- TF[,pcols,with=FALSE]
       TF <- TF[Code %in% ft$StartCode:ft$EndCode]
+      TF <- TF[!(is.na(Grams) & is.na(Kilos) & is.na(Expenditure) & is.na(Price))] 
       
       TF[,Price:=as.numeric(Price)]
       TF[,Kilos:=as.numeric(Kilos)]
       TF[,Grams:=as.numeric(Grams)]
       TF[,Expenditure:=as.numeric(Expenditure)]
       
-      TF[,Code:=NULL]
-      TF[is.na(TF)] <- 0
-      TF[,FGrams:=(Kilos*1000+Grams)/30]
+      TF[is.na(Grams) & !is.na(Kilos),Grams:=0]
+      TF[is.na(Kilos) & !is.na(Grams),Kilos:=0]
+      TF[,FGrams:=(Kilos*1000+Grams)]
     }
+    
+    TF[is.na(Price), Price:= Expenditure/(FGrams/1000)]
+    TF[is.na(FGrams),FGrams:=Expenditure/Price*1000]
+    
+    TF[,Code:=NULL]
+    TF[is.na(TF)] <- 0
     
     FData <- TF[,lapply(.SD,sum),by=HHID]
+    FData[!is.na(FGrams), Price:=Expenditure/(FGrams/1000)]
     FData[, FoodType:=TFoodGroups[i,FoodType]]
-    FData[, FoodKCalories:=TFoodGroups[i,KCalories]*FGrams]
-    FData[, FoodProtein:=TFoodGroups[i,Protein]*FGrams]
+    FData[, FoodKCalories:=TFoodGroups[i,KCalories]*FGrams/30]
+    FData[, FoodProtein:=TFoodGroups[i,Protein]*FGrams/30]
+    FData[is.infinite(Price),Price:=NA]
     
-    BigFData <- rbind(BigFData,FData[,.(HHID,FGrams,Expenditure,FoodType,FoodKCalories,FoodProtein)])
+    BigFData <- rbind(BigFData,
+                      FData[,.(HHID,FoodType,Price,Expenditure,FGrams,FoodKCalories,FoodProtein)])
     save(BigFData, file = paste0(Settings$HEISProcessedPath,"Y",year,"BigFData.rda"))
-    cat(BigFData[,mean(FoodKCalories)],"\t")
-    cat(BigFData[,mean(FoodProtein)],"\n")
     }
 }
-cat("\n\n==============Finish==============\nIt took ")
 endtime <- proc.time()
-cat((endtime-starttime)[3],"seconds.")
+cat("\n\n==============Finish==============\nIt took ",(endtime-starttime)[3],"seconds.")
