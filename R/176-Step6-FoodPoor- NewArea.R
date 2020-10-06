2856878f954e3080710c45c0fe2b2cc8292ed33e
Arin Shahbazian <arin1989@gmail.com>
2020-10-06 20:48:21 +0330

    Creating new files for clustering

---
+++ R/176-Step6-FoodPoor- NewArea.R
@@ -0,0 +1,73 @@
+# 166-Step6-FoodPoor.R: Calculate base year basket cost in current year prices 
+#                  for each cluster. and find FoodPoor
+#
+# Copyright © 2019-2020: Arin Shahbazian & Majid Einian
+# Licence: GPL-3
+
+rm(list=ls())
+starttime <- proc.time()
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+library(readxl)
+library(data.table)
+library(spatstat)
+year<-Settings$baseBundleyear
+
+load(file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoorClustered.rda"))
+load(file=paste0(Settings$HEISProcessedPath,"Y",year,"BigFData.rda"))
+
+MD[,Selected_Group:=ifelse((Region=="Urban" & Decile==3) |
+                             (Region=="Rural" & Decile==2),1,0)]
+
+Bfd2 <- data.table(expand.grid(HHID=MD$HHID,FoodType=unique(BigFData$FoodType)))
+Bfd2 <- merge(Bfd2,BigFData,all.x = TRUE)
+Bfd2 <- merge(Bfd2,MD[,.(HHID,Region,Weight,Size,
+                         EqSizeCalory,Selected_Group)],by="HHID")
+Bfd2[is.na(Bfd2)]<-0
+Bfd2[Price<0.1,Price:=NA]
+
+BaseYearBasket <- Bfd2[Selected_Group==1,
+                       .(FGramspc=weighted.mean(FGrams/EqSizeCalory,Weight*Size),
+                         FKCalspc=weighted.mean(FoodKCalories/EqSizeCalory,Weight*Size)),
+                       by=.(FoodType,Region)]
+BaseYearBasket[,BasketCals:=sum(FKCalspc),by=Region]
+BaseYearBasket[,StandardFGramspc:=FGramspc*Settings$KCaloryNeed_Adult_WorldBank/BasketCals]
+
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoorClustered.rda"))
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"BigFData.rda"))
+  
+  MD[,Selected_Group:=ifelse((Decile==1) | (Decile==2) |
+                               (Decile==3) | (Decile==4),1,0)]
+  
+  Bfd2 <- data.table(expand.grid(HHID=MD$HHID,FoodType=unique(BigFData$FoodType)))
+  Bfd2 <- merge(Bfd2,BigFData,all.x = TRUE)
+  Bfd2 <- merge(Bfd2,MD[,.(HHID,Region,Weight,Size,NewArea_Name,
+                           EqSizeCalory,Selected_Group)],by="HHID")
+  Bfd2[is.na(Bfd2)]<-0
+  Bfd2[Price<0.1,Price:=NA]
+  # Basket <- Bfd2[Selected_Group==1,
+  #                .(FGramspc=weighted.mean(FGrams/EqSizeCalory,Weight*Size)),
+  #                by=.(FoodType,NewArea_Name)]
+  
+  BasketPrice <- Bfd2[Selected_Group==1 & !is.na(Price),
+                      .(Price=weighted.median(Price,Weight*Size,na.rm = TRUE)),
+                      by=.(FoodType,Region,NewArea_Name)]
+  
+  BasketCost <- merge(BaseYearBasket,BasketPrice,by=c("FoodType","Region"))
+  BasketCost[,Cost:=(StandardFGramspc/1000)*Price]
+  FPLineBasket <- BasketCost[,.(FPLine=sum(Cost)),by=NewArea_Name]
+  
+  MD <- merge(MD,FPLineBasket,all.x=TRUE,by="NewArea_Name")
+  
+  MD[,FoodPoor:=ifelse(TOriginalFoodExpenditure_Per < FPLine,1,0)]
+
+
+  save(MD,file=paste0(Settings$HEISProcessedPath,"Y",year,"FoodPoor2.rda"))
+}
+
+endtime <- proc.time()
+cat("\n\n============================\nIt took",
+    (endtime-starttime)["elapsed"]," seconds")
---
+++ R/177-Step7-Engel- NewArea.R
@@ -0,0 +1,94 @@
+# 167-Step7-Engel : Calculated Engel and modified Engel and Poverty Lines
+# 
+# Copyright © 2018-2020: Majid Einian & Arin Shahbazian
+# Licence: GPL-3
+
+rm(list=ls())
+
+starttime <- proc.time()
+cat("\n\n================ Engel =====================================\n")
+library(yaml)
+Settings <- yaml.load_file("Settings.yaml")
+
+library(readxl)
+library(data.table)
+
+BigEngelTable <- data.table(Region=NA_character_,NewArea_Name=NA_integer_,
+                            N=NA_integer_,Engel=NA_real_,
+                            FPLine=NA_real_,Year=NA_integer_)[0]
+
+for(year in (Settings$startyear:Settings$endyear)){
+  cat(paste0("\nYear:",year,"\t"))
+  
+  load(file=paste0(Settings$HEISProcessedPath,"Y",year,"FoodPoor2.rda"))
+  
+  MD<-MD[,EngelH:=(TOriginalFoodExpenditure/Total_Exp_Month)]
+  
+  
+  EngelD <- MD[ TOriginalFoodExpenditure_Per>0.8*FPLine &
+                   TOriginalFoodExpenditure_Per<1.2*FPLine,
+                 .(.N,Engel=weighted.mean(EngelH,Weight),
+                   FPLine=mean(FPLine))
+               ,by=.(Region,NewArea_Name)]
+  
+  save(EngelD,file=paste0(Settings$HEISProcessedPath,"Y",year,"EngelD.rda"))
+
+  EngelD<-EngelD[,Year:=year]
+  BigEngelTable <- rbind(BigEngelTable,EngelD)
+}
+
+InflationData <- data.table(read_excel(path = Settings$InflationDataFilePath))
+InflationData[,F1 := (1+D2FoodInf)/(1+D2Inf)]
+InflationData<-InflationData[order(Year)]
+InflationData[,l.F1:=data.table::shift(F1)]
+InflationData[,F2 := F1*l.F1]
+
+
+BigEngelTable<-merge(BigEngelTable,InflationData,by="Year")
+BigEngelTable<-BigEngelTable[order(Year,NewArea_Name)]
+BigEngelTable[,l.Engel:=data.table::shift(Engel),by=NewArea_Name]
+BigEngelTable[,l2.Engel:=data.table::shift(Engel,2),by=NewArea_Name]
+
+BigEngelTable[,EngelX:=l.Engel*F1]
+BigEngelTable[,EngelX2:=l2.Engel*F2]
+
+
+BigEngelTable[is.na(EngelX) & is.na(EngelX2),ModifiedEngel:=Engel]
+BigEngelTable[!is.na(EngelX) & is.na(EngelX2),ModifiedEngel:=(Engel+EngelX)/2]
+BigEngelTable[is.na(ModifiedEngel),ModifiedEngel:=(Engel+EngelX+EngelX2)/3]
+BigEngelTable[,PovertyLine:=FPLine/ModifiedEngel]
+BigEngelTable[,PovertyLine0:=FPLine/Engel]
+
+save(BigEngelTable,file=paste0(Settings$HEISProcessedPath,"BigEngelTable2.rda"))
+
+
+# btm <- melt(BigEngelTable,id.vars = c("Year","NewArea_Name","Region"),
+#             measure.vars = c("Engel","ModifiedEngel"))
+# 
+# library(ggplot2)
+# library(gridExtra)
+# library(ggpubr)
+# 
+# plotlist <- list()
+# for(cl in 1:13){
+#   plt = ggplot(btm[NewArea_Name==cl,], 
+#                aes(x=Year,y=value,
+#                    fill=variable,color=variable,linetype=variable)) +
+#     geom_line() +
+#     geom_point() +
+#     annotate(geom="label", x=84, y=max(btm[NewArea_Name==cl,value])-.01, label=cl, color="red")+
+#     theme(legend.position = "none",
+#           axis.title.x=element_blank(),
+#           axis.title.y=element_blank())
+#   
+#   plotlist[[cl]] <- plt
+# }
+# plt = ggplot(btm[NewArea_Name==cl,], aes(x=Year,y=value,fill=variable,color=variable)) +
+#   geom_line() +
+#   geom_point()
+# leg <- get_legend(plt)
+# plotlist[[cl+1]] <- as_ggplot(leg)
+# do.call("grid.arrange", c(plotlist, ncol=2))
+
+endtime <- proc.time()
+cat("\n\n============================\nIt took",(endtime-starttime)["elapsed"],"seconds")
